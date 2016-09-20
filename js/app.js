0eababf3d8303af4f417b1cfe49c9ec49975ce1a
krystalcampioni <krystal.designer@gmail.com>
2016-09-19 21:15:19 -0300

    implemented two enemies. TODO: randomize number of enemies and their position

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -11,9 +14,11 @@ var Enemy = function() {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  this.x += this.speed * dt;
+
+  if (this.x >= 505) {
+      this.x = 0;
+  }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -62,7 +67,11 @@ Player.prototype.handleInput = function(key) {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var allEnemies = [];
+var enemy = new Enemy(-150, 280, 50);
+var enemy2 = new Enemy(-50,0, 250);
 var player = new Player(200, 380, 50);
+
+allEnemies.push(enemy, enemy2);
 
 
 // This listens for key presses and sends the keys to your
